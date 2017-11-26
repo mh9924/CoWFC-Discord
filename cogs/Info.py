f094ca0d2875a64b099255155aa12e6c8805edc2
Matt <xmatth13@live.com>
2017-11-26 02:44:48 -0500

    Code cleanup

--- cogs/Info.py
+++ cogs/Info.py
@@ -8,7 +8,7 @@ class Info:
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def stats(self):
 		r = requests.get(self.bot.config['info']['dwc_stats_page'])
 		soup = BeautifulSoup(r.content, "html.parser")
@@ -20,11 +20,11 @@ class Info:
 		message = "There are currently "+numusers+" out of max 12 players online. ("+lastupdate+")"
 		await self.bot.say(message)
 		
-	@commands.command(pass_context=True)
+	@commands.command()
 	async def errors(self):
 		r = requests.get(self.bot.config['info']['errors_page'])
 		soup = BeautifulSoup(r.content, "html.parser")
-		message = ''
+		message = []
 		for error in soup.findAll('li', attrs={'id':'error'}):
-			message += error.getText()+'\n'
-		await self.bot.say(message)
+			message.append(error.getText())
+		await self.bot.say('\n'.join(message))
