f24092a23f673af86670dd2712f5ec5008638f75
Matt <xmatth13@live.com>
2018-03-23 12:48:30 -0400

    sp

--- cogs/Info.py
+++ cogs/Info.py
@@ -13,11 +13,14 @@ class Info:
 		r = requests.get(self.bot.config['info']['dwc_stats_page'])
 		soup = BeautifulSoup(r.content, "html.parser")
 		lastupdate = soup.find('i').getText()
+		
 		if soup.find('center') is None:
 			numusers = '0'
 		else:
 			numusers = soup.find('center').getText()
+			
 		message = "There are currently "+numusers+" out of max 12 players online. ("+lastupdate+")"
+		
 		await self.bot.say(message)
 		
 	@commands.command()
@@ -25,6 +28,8 @@ class Info:
 		r = requests.get(self.bot.config['info']['errors_page'])
 		soup = BeautifulSoup(r.content, "html.parser")
 		message = []
+		
 		for error in soup.findAll('li', attrs={'id':'error'}):
 			message.append(error.getText())
+			
 		await self.bot.say('\n'.join(message))
